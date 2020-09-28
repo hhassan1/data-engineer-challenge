fb9a0dcf6ccf496e1a7db699b2580554c8a5e4bb
Hussein Hassan Harrirou <hhassan@protonmail.ch>
2020-09-28 23:55:19 +0200

    Add consumer

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ export INPUT_TOPIC=input_topic
 export OUTPUT_TOPIC=output_topic
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+pip install -r pip-requirements
 docker-compose down --volumes
 docker-compose up -d
 
@@ -20,6 +21,7 @@ sleep 15
 
 ./data-generator -c ${LINES} -r ${RATE} -n ${UNIQUE_USERS} -d ${DELAY} -p ${PROB_DELAY} | ./kafka-producer.py --broker localhost:${PORT} --topic ${INPUT_TOPIC} &
 ./measure_stream_statistics.py --broker localhost:${PORT} --input ${INPUT_TOPIC} --output ${OUTPUT_TOPIC} --delay ${DELAY} &
+./kafka-consumer.py --broker localhost:${PORT} --topic ${OUTPUT_TOPIC} &
 
 sleep infinity
 
